c5fa51a16d25541dd73f741936336179d12131b6
meiming <meiming@bigwei.com>
2018-04-11 16:49:38 +0800

    test address to long

--- src/main/java/com/inschos/proposal/controller/request/WarrantyController.java
+++ src/main/java/com/inschos/proposal/controller/request/WarrantyController.java
@@ -29,7 +29,7 @@ public class WarrantyController {
     @RequestMapping("/insureTest")
     @ResponseBody
     public String insureTest(HttpServletRequest request){
-        String body = "{\"channel_code\":\"YD\",\"insured_name\":\"张耘赫\",\"insured_code\":\"620103199012171917\",\"insured_phone\":\"13186050625\",\"insured_email\":\"wangs@inschos.com\",\"insured_province\":\"北京市\",\"insured_city\":\"北京市\",\"insured_county\":\"东城区\",\"insured_address\":\"夕照寺中街19号\",\"bank_name\":\"工商银行\",\"bank_code\":\"6217730704649985 \",\"bank_phone\":\"13186050625\",\"bank_address\":\"北京市东城区广渠门内广渠路支行\",\"insured_days\":1,\"price\":2}";
+        String body = "{\"channel_code\":\"YD\",\"insured_name\":\"张耘赫\",\"insured_code\":\"620103199012171917\",\"insured_phone\":\"13186050625\",\"insured_email\":\"wangs@inschos.com\",\"insured_province\":\"北京市\",\"insured_city\":\"北京市\",\"insured_county\":\"东城区\",\"insured_address\":\"北京市东城区广渠门内广渠路夕照寺中街19号幸福大厦15单元17号楼东门对面的平房后面的小黑屋里\",\"bank_name\":\"工商银行\",\"bank_code\":\"6217730704649985 \",\"bank_phone\":\"13186050625\",\"bank_address\":\"北京市东城区广渠门内广渠路支行\",\"insured_days\":1,\"price\":2}";
         return warrantyAction.insureTest(body);
     }
 
