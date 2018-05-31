57545a69b8c3db86859e70fa1ab8eeb4f1295631
meiming <meiming@bigwei.com>
2018-05-31 15:52:45 +0800

    reote

--- src/main/java/com/inschos/proposal/remote/action/InsureRemote.java
+++ src/main/java/com/inschos/proposal/remote/action/InsureRemote.java
@@ -178,7 +178,7 @@ public class InsureRemote {
 
 
         String displayName = Charset.defaultCharset().displayName();
-        String head = String.format(bodyHead, displayName);
+        String head = String.format(bodyHead, "GBK");
         L.log.debug("head {}", head);
         return head + XmlKit.bean2Xml(request);
     }
@@ -246,7 +246,7 @@ public class InsureRemote {
         tyInsProposalRequest.mainDto.insuredDtoList.add(insuredDto);
 
         String displayName = Charset.defaultCharset().displayName();
-        String head = String.format(bodyHead, "GBK");
+        String head = String.format(bodyHead, displayName);
         L.log.debug("head {}", head);
         return head + XmlKit.bean2Xml(tyInsProposalRequest);
 
